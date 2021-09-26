fa13afbd4d4c5584e757d250306bd11733ca4eb0
freddneos <fredd@neosdev.com.br>
2021-09-26 18:42:12 +0100

    chore:show info mobile

--- src/components/ShowInfo/styled.ts
+++ src/components/ShowInfo/styled.ts
@@ -1,5 +1,7 @@
 import styled from "styled-components";
 import Image from 'next/image'
+import breakpoint from '../../styles/breakpoints'
+
 
 export const Container = styled.div`
   display: flex;
@@ -33,12 +35,55 @@ export const Container = styled.div`
     font-size: 14px;
     font-weight: 200;
   }
+
+  @media only screen and ${breakpoint.device.sm}{
+    align-items: center;
+    justify-content: center;
+    flex-direction: column;
+    min-height: 700px;
+    padding-top: 50px;
+
+    h2 {
+      margin-top: 10px;
+      font-size: 24px;
+      font-weight: 600;
+      padding-left: 20px;
+      padding-bottom: 20px;
+    }
+
+    ul{
+      padding-top: 10px;
+      padding-bottom: 40px;
+      list-style-type:none;
+      overflow-y: auto;
+      overflow-x: hidden;
+
+      li {
+        font-size: 12px;
+        margin: 10px;
+        width: 100%;
+        display: flex;
+        align-items: center;
+        b {
+          width: 100px;
+          padding-left: 10px;
+          }
+        }
+    }
+}
 `
 
 export const ShowInfoContainer = styled.div`
   display: flex;
   flex-direction: column;
   align-items: flex-start;
+  padding-left: 50px;
+  @media only screen and ${breakpoint.device.sm}{
+      padding-left: 0px;
+      width: 100%;
+  }
+
+
 `
 export const ImageStyled = styled(Image)`
   border-radius: 50%;
